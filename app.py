8adfcd756f5b2de514acd7e7797e30ade76988b1
Aayush Raj <aayushr2201@gmail.com>
2025-06-05 13:30:06 +0530

    update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import numpy as np
 import tensorflow as tf
-from tensorflow import keras
+import tensorflow as tf
+from tensorflow.keras.models import load_model
 from PIL import Image
 import matplotlib.pyplot as plt
 
